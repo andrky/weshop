7ab6ebd7dac1610921da56e51440d7ad1a2ef0dc
andrky <anandarizky166@gmail.com>
2022-03-10 15:02:56 +0700

    Add File Login and Proses Login

--- login.php
+++ login.php
@@ -1,23 +1,24 @@
 <?php
 $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
-if ($notif == "require") {
+if ($notif == "true") {
   echo "<p id='notif'>
                 Maaf, e-mail dan password anda tidak cocok!
               </p>";
-} 
+}
 ?>
 
 <div id="form">
   <form action="<?php echo BASE_URL . "proses_login.php"; ?>" method="POST">
+    <div class="mb-3">
       <label for="E-mail" class="form-label">E-mail</label>
-      <input type="text" class="form-control" id="E-mail" name="email" value="<?php echo $email ?>">
+      <input type="text" class="form-control" id="E-mail" name="email">
     </div>
     <div class="mb-3">
       <label for="Password" class="form-label">Password</label>
       <input type="password" class="form-control" id="Password" name="password">
     </div>
     <div class="col-12">
-      <button type="submit" class="btn" id="btn-login">Login</button>
+      <button type="submit" class="btn" id="btn-register">Login</button>
     </div>
   </form>
 </div>
